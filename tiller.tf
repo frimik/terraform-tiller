68ce7558cdaffe4c35f08e593705d99d56e8fb8d
Ollie Thomas <ollie.thomas1992@gmail.com>
2019-03-08 14:59:12 +0000

    changed deployment template

--- tiller.tf
+++ tiller.tf
@@ -122,8 +122,8 @@ resource "kubernetes_deployment" "tiller" {
             timeout_seconds       = 1
           }
         }
-        automount_service_account_token = true
-        service_account = "tiller"
+        # automount_service_account_token = true
+        # service_account = "tiller"
         service_account_name  = "tiller"
         host_network  = "${var.tiller_net_host}"
         node_selector = "${var.tiller_node_selector}"
